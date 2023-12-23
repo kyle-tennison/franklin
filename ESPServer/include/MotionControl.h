e46c741e568ebd9896d7c2b19e70c569fd3acb8c
Kyle Tennison <kyletennison05@gmail.com>
2023-12-22 22:23:19 -0800

    gyroscope I2C

--- ESPServer/include/MotionControl.h
+++ ESPServer/include/MotionControl.h
@@ -8,13 +8,154 @@ Runs in core zero along with WebsocketServer.
 */
 
 #include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+#define MPU_I2C_ADDR 0x68
+#define I2C_CLOCK_SPEED 400000
+
+#define ROT_VARIANCE_GYRO 4
+#define ROT_VARIANCE_ACCEL 3
+
+#define ALPHA 0.125
+
+#define GYRO_POLL_DELAY 100
+struct GyroState {
+  float theta_x;
+  float theta_y;
+};
+
+struct GyroRecord{
+  float theta_x;
+  float theta_y;
+  uint32_t timestamp;
+  float uncertainty;
+};
+
+struct Angles {
+  float theta_x;
+  float theta_y;
+};
+
+GyroRecord gyro_record;
+
+void setup_gyro(){
+
+  // give gyro_record starting values
+  gyro_record.theta_x = 0;
+  gyro_record.theta_y = 0;
+  gyro_record.timestamp = micros();
+  gyro_record.uncertainty = 0;
+
+  Wire.setClock(I2C_CLOCK_SPEED);
+  Wire.begin();
+  delay(250); 
+
+  // i have no clue what this does
+  Wire.beginTransmission(0x68);
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission();
+
+  // Turn on low-pass to filter out vibrations
+  Wire.beginTransmission(MPU_I2C_ADDR);
+  Wire.write(0x1A); // CONFIG register
+  Wire.write(0b10); // filter out 94hz (pg. 13)
+  Wire.endTransmission();
+
+  // Get accel results as multiples of 4g
+  Wire.beginTransmission(MPU_I2C_ADDR);
+  Wire.write(0x1C); // ACCEL_CONFIG register
+  Wire.write(0b00010000); // ASF_SEL = 2 (pg. 15)
+  Wire.endTransmission();
+
+  // Get angular rate in 500°/s
+  Wire.beginTransmission(MPU_I2C_ADDR);
+  Wire.write(0x1B); // GYRO_CONFIG register
+  Wire.write(0b00001000); // FS_SEL = 1 (pg. 14)
+  Wire.endTransmission();
+
+}
+
+Angles angle_from_accel(float accel_x, float accel_y, float accel_z){
+
+  float theta_y = atan2(accel_x, accel_z) * RAD_TO_DEG;
+  float theta_x = atan2(accel_y, accel_z) * RAD_TO_DEG;
+
+  return Angles{
+    theta_x,
+    theta_y
+  };
+}
+
+
+Angles poll_gyro(){
+
+  // Read accelerometer data
+  Wire.beginTransmission(MPU_I2C_ADDR);
+  Wire.write(0x3B); // set ACCEL_XOUT_H as starting register
+  Wire.endTransmission();
+  Wire.requestFrom(MPU_I2C_ADDR, 6); // read ACCEL_XOUT_H and the five registers after
+
+  // combine bytes from each register pair
+  int16_t accel_x_raw = Wire.read() << 8 | Wire.read();
+  int16_t accel_y_raw = Wire.read() << 8 | Wire.read(); 
+  int16_t accel_z_raw = Wire.read() << 8 | Wire.read();
+
+  // convert from LSB to g
+  float accel_x = (float)accel_x_raw / 4096;
+  float accel_y = (float)accel_y_raw / 4096;
+  float accel_z = (float)accel_z_raw / 4096;
+
+  // read gyro data
+  Wire.beginTransmission(MPU_I2C_ADDR);
+  Wire.write(0x43); // set omega_xOUT_H as starting register
+  Wire.endTransmission();
+  Wire.requestFrom(MPU_I2C_ADDR, 6); // read omega_xOUT_H and the five registers after
+
+  // combine bytes from each register pair
+  int16_t omega_x_raw = Wire.read() << 8 | Wire.read();
+  int16_t omega_y_raw = Wire.read() << 8 | Wire.read();
+  int16_t omega_z_raw = Wire.read() << 8 | Wire.read();
+
+  // convert from LSB to °/sec
+  float omega_x = (float)omega_x_raw / 65.5;
+  float omega_y = (float)omega_x_raw / 65.5;
+  float omega_z = (float)omega_x_raw / 65.5;
+
+  Angles accel_angle = angle_from_accel(accel_x, accel_y, accel_z);
+
+  // kalman filter to combine accelerometer and gyroscope data
+  float theta_x = ALPHA * (accel_angle.theta_x + omega_x) + (1 - ALPHA) * accel_angle.theta_x;
+  float theta_y = ALPHA * (accel_angle.theta_y + omega_y) + (1 - ALPHA) * accel_angle.theta_x;
+
+
+  return Angles {
+    theta_x,
+    theta_y
+  };
+
+}
+
 
 /// @brief interprets sensor inputs and pre-processes for MotorDrive.h
 /// @param _ unused
 void telemetry_loop(void* _){
 
+    // let websocket start first
+    delay(5000);
+
+    setup_gyro();
+
     while (1){
-        delay(10);
+
+        Angles angles = poll_gyro();
+
+        Serial.println(angles.theta_x);
+
+
+        delay(GYRO_POLL_DELAY);
     }
 
 }
