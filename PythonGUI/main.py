d168b1164f96e0040745ee6a4579291410d4fba9
Kyle Tennison <kyletennison05@gmail.com>
2024-01-29 21:31:03 -0800

    cleanup python

--- PythonGUI/main.py
+++ PythonGUI/main.py
@@ -1,31 +1,39 @@
-import graphs
+"""
+Entry Point
 
-window = graphs.Window()
+Starts socket server and opens window when ready.
+
+January 2024
+"""
+
+import gui
+
+window = gui.Window()
 window.add_graph(
-    graphs.Graph(
-        window=window, 
-        name="Gyroscope", 
-        placement=(1,3,1),
+    gui.Graph(
+        window=window,
+        name="Gyroscope",
+        placement=(1, 3, 1),
         poller=lambda: window.status.status.gyro_value,
-        limits=(-45, 45)
-        )
+        limits=(-45, 45),
+    )
 )
 window.add_graph(
-    graphs.Graph(
-        window=window, 
-        name="Motor Target", 
-        placement=(1,3,2),
+    gui.Graph(
+        window=window,
+        name="Motor Target",
+        placement=(1, 3, 2),
         poller=lambda: window.status.status.motor_target,
-        limits=(-55, 55)
-        )
+        limits=(-55, 55),
+    )
 )
 window.add_graph(
-    graphs.Graph(
-        window=window, 
-        name="Integral Sum", 
-        placement=(1,3,3),
+    gui.Graph(
+        window=window,
+        name="Integral Sum",
+        placement=(1, 3, 3),
         poller=lambda: window.status.status.integral_sum,
-        limits=(-12, 12)
-        )
+        limits=(-110, 110),
+    )
 )
 window.show()
--- PythonGUI/socket_server.py
+++ PythonGUI/socket_server.py
@@ -1,19 +1,28 @@
-from dataclasses import dataclass
-import json
-from pprint import pprint
-import socket
-import time
-from typing import Optional
-from pydantic import BaseModel, Field
+"""
+Socket Server
+
+Handles connection stream with Rust client.
+
+January 2024
+"""
+
+
+import os
 import csv
+import json
+import uuid
+import socket
+from typing import Any, Optional
+from pydantic import BaseModel, Field
 
-LOGFILE = "log.csv"
+LOGFILE = f"{uuid.uuid4()}_log.csv"
 
-@dataclass
-class OperationRequest:
+
+class OperationRequest(BaseModel):
     code: int
     content_length: int
     content: bytes
+
 
 class StatusResponse(BaseModel):
     pid_integral: float = Field(alias="PID Integral", default=0)
@@ -27,60 +36,73 @@ class StatusResponse(BaseModel):
 
 
 class Server:
+    """A socket server for streaming franklin telemetry"""
 
     def __init__(self) -> None:
         self.sock = socket.socket()
-
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind(("localhost", 2999))
-        self.connect()
+        self.wait_for_connection()
 
-    def connect(self):
-        print("waiting for connection...")
+    def wait_for_connection(self) -> None:
+        """Waits for an incoming socket connection. Saves connection to self.con"""
+
+        print("info: waiting for connection...")
         self.sock.listen(1)
         self.con, addr = self.sock.accept()
-        print("made connection")
+        print(f"info: made connection with {self.con.getpeername()}")
         self.has_alive_connection = True
 
     def accept_incoming(self) -> Optional[OperationRequest]:
-        recv = b''
-        while recv == b'':
+        """Accepts an incoming packet. Blocks until a packet is found.
+
+        Returns:
+            An OperationRequest object of the incoming request, or None if the
+            packet is invalid.
+        """
+
+        recv = b""
+        while recv == b"":
             recv = self.con.recv(1)
 
-        if recv == b'F':
-
+        if recv == b"F":
             header = self.con.recv(4)
 
             if header[0] != 70:
                 print(f"error: bad header; {int(header[0])} != 70")
                 return
-            
-            operation = header[1]
 
+            # process the header
+            operation = header[1]
             c1 = header[2]
             c2 = header[3]
-
             content_length = (c1 << 8) + c2
-
-            print("debug: content length is", content_length)
 
             content = self.con.recv(content_length)
 
             return OperationRequest(
-                code=operation,
-                content_length=content_length,
-                content=content
+                code=operation, content_length=content_length, content=content
             )
-        
+
         else:
-            # keep looking for a header byte
-            return
-        
-        
-    def dispatch_operation(self, operation_request: OperationRequest):
+            print(f"warn: {int(recv)} is not a header byte")
+            return None
+
+    def dispatch_operation(self, operation_request: OperationRequest) -> Optional[Any]:
+        """Dispatches an operation to its corresponding handler.
+
+        Args:
+            operation_request: The OperationRequest object containing the
+                request.
+
+        Returns:
+            A relay of the dispatched output, it it exists.
+        """
 
         if operation_request.code == 0:
-            print(f"info: incoming message -- '{operation_request.content.decode('utf-8')}'")
+            print(
+                f"info: incoming message -- '{operation_request.content.decode('utf-8')}'"
+            )
 
         elif operation_request.code == 1:
             self.con.close()
@@ -89,7 +111,9 @@ class Server:
 
         elif operation_request.code == 2:
             print("incoming json")
-            incoming = StatusResponse(**json.loads(operation_request.content.decode("utf-8")))
+            incoming = StatusResponse(
+                **json.loads(operation_request.content.decode("utf-8"))
+            )
             self.log_status(incoming)
             print(incoming.model_dump_json(indent=4))
             return incoming
@@ -97,24 +121,37 @@ class Server:
         else:
             print(f"error: unknown operation {operation_request.code}")
 
-    def log_status(self, incoming: StatusResponse):
+    def log_status(self, incoming: StatusResponse) -> None:
+        """Logs a status message to the LOGFILE. Will create the file if
+        it does not already exist and will add headers if not present.
+
+        Args:
+            incoming: The status to append to the log
+        """
+
+        if not os.path.exists(LOGFILE):
+            open(LOGFILE, "w").close()
 
         has_headers = True
-        with open(LOGFILE, 'r') as f:
+        with open(LOGFILE, "r") as f:
             if "motor_target" not in f.read():
                 has_headers = False
 
         dict_buffer = incoming.model_dump()
 
-        with open(LOGFILE, 'a') as f:
+        with open(LOGFILE, "a") as f:
             w = csv.DictWriter(f, dict_buffer.keys())
             if not has_headers:
                 w.writeheader()
             w.writerow(dict_buffer)
 
-
     def wait_for_update(self) -> StatusResponse:
-        print("waiting for update...")
+        """Waits for an incoming update message.
+
+        Returns:
+            The deserialized StatusResponse of the update
+        """
+        print("info: waiting for update...")
         while True:
             while self.has_alive_connection:
                 operation = self.accept_incoming()
@@ -125,25 +162,5 @@ class Server:
                 if operation.code == 2 and isinstance(result, StatusResponse):
                     return result
 
-            self.connect()
-
-
-
-
-if __name__ == "__main__":
-    server = Server()
-    try:
-        while True:
-            while server.has_alive_connection:
-                operation = server.accept_incoming()
-                if operation is None:
-                    continue
-                server.dispatch_operation(operation)
-            server.connect()
-
-    except:
-        print("closing socket")
-        server.con.close()
-        server.sock.close()
-        raise
-
+            print("warning: client disconnected, attempting to reconnect")
+            self.wait_for_connection()
